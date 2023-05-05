6678c44770353a1a2b43e9b3c4d7bf81e83c36af
Sandun Tharuka <sandun.suntws@gmail.com>
2023-05-05 17:06:19 +0530

    setup pages

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {FiSettings} from 'react-icons/fi'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+const App = () => {
+
+const activeMenu =false
+
+
+
+  return (
+    <div>
+<BrowserRouter>
+<div className='flex relative dark:bg-main-dark-bg'> 
+<div className='fixed right-4 bottom-4 ' style={{zIndex:'1000'}}>
+<TooltipComponent content='Settings' position='Top' >
+<button type='button'  className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray" style={{backgroundColor:'blue'}}> 
+  <FiSettings/>
+</button>
+</TooltipComponent>
+</div>
+{activeMenu?(<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+  Sidebar
+</div>):(<div className='w-0 dark:bg-secondary-dark-bg'>
+  Sidebar 0
+</div>)}
+
+<div className={ `dark:bg-main-bg bg-main-bg min-h-screen  w-full ${activeMenu?' md:ml-72 ':'flex-2'}`}>
+<div className='fixeds md:static bg-main-bg dark:bg-main-dark-bg navb ar w-full'> 
+nav
+</div>
+</div>
+
+
+<div>
+
+<Routes>
+{/* dashborad */}
+<Route path='/' element="Ecommerce"/>
+<Route path='/ecommerce' element="Ecommerce"/>
+
+{/* Pages */}
+
+<Route path='/orders' element="Orders"/>
+<Route path='/employees' element="Employees"/>
+<Route path='/customers' element="Customers"/>
+
+{/* apps */}
+
+<Route path='/kanban' element="Kanban"/>
+<Route path='/editor' element="Editor"/>
+<Route path='/calendar' element="Calendar"/>
+<Route path='/color-picker' element="ColorPicker"/>
+
+{/* charts */}
+
+<Route path='/line' element="Line"/>
+<Route path='/area' element="Area"/>
+<Route path='/bar' element="Bar"/>
+<Route path='/pie' element="Pie"/>
+<Route path='/financial' element="Financial"/>
+<Route path='/color-mapping' element="Pyramid"/>
+<Route path='/pyramid' element="Pyramid"/>
+<Route path='/stacked' element="Stacked"/>
+
+
+
+</Routes>
+
+</div>
+
+
+
+
+</div>
+
+
+
+</BrowserRouter>
+
+    </div>
+  )
+}
+
+export default App
